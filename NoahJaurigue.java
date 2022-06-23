eb2a0453c10bb126c8fdedc000b16e043caf3bfb
njaurigue <noahdj37@gmail.com>
2022-06-23 15:20:39 -0700

    Section Four additions

---
+++ NoahJaurigue.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.List;
+
+class NoahJaurigue{
+    static List<String> getInContact(){
+        List<String> myContacts = 
+            Arrays.asList(
+                "Email: noahdj37@gmail.com",
+                "GitHub: github.com/njaurigue",
+                "LinkedIn: linkedin.com/in/noah-jaurigue/",
+                "Instagram: @noahjaurigue"
+            );
+        return myContacts;
+    }
+}
+
+
