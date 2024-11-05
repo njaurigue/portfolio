30c56f03b82aa3b687cfc443c51758761651c52c
Noah Jaurigue <noahjaurigue@Noahs-MacBook-Pro.local>
2024-11-04 20:20:32 -0800

    update courses etc.

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
 function copyTo(){
-    navigator.clipboard.writeText("noahdj37@gmail.com").then(() => {
-        // Alert the user that the action took place.
-        // Nobody likes hidden stuff being done under the hood!
-        alert("Email (noahdj37@gmail.com) copied to clipboard!");
-    });
+    navigator.clipboard.writeText("noahdj37@gmail.com");
+    alert("Email (noahdj37@gmail.com) copied to clipboard!");
 }
